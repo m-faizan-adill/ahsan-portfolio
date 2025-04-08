ccaf7476aced1f271a0f4dcab158966fc8d06baf
Muhammad Faizan Adil <adilmuhammadfaizan@gmail.com>
2025-04-09 02:44:46 +0500

    wrapped the theme provider in main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 import { TempoDevtools } from "tempo-devtools";
+import { ThemeProvider } from "./components/providers/ThemeProvider.tsx";
 TempoDevtools.init();
 
 const basename = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <StrictMode>
+    <ThemeProvider>
+      <BrowserRouter basename={basename}>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>,
 );
